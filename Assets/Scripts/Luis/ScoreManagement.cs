ed901b82ca0b146345179335b6721b0cb5d072d2
bechara97 <bechara.l@husky.neu.edu>
2019-02-11 15:27:38 -0500

    Tower Light and Score all set Including UI for display

---
+++ Assets/Scripts/Luis/ScoreManagement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManagement : MonoBehaviour
+{
+    public static int kills;
+    public static float secondsSurvived;
+    float timer = 0.0f;
+    float seconds = 0;
+    public Text scoreLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountTime();
+        scoreLabel.text = "Score: " + this.FinalScore();
+    }
+
+    void CountTime()
+    {
+        timer += Time.deltaTime;
+        seconds = timer % 60;
+    }
+
+    public float FinalScore(){
+        return secondsSurvived * (kills+1);
+    }
+}
---
+++ Assets/Scripts/Luis/Tower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tower : MonoBehaviour
+{
+    public static float health;
+    public Text healthLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthLabel.text = "Tower Health: " + health;
+    }
+}
